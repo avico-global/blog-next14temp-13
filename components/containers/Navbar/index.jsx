d1bc0b002c3680a4ec532c271ef01e0c940d382b
Usama <usamamustafa393@gmail.com>
2025-03-12 22:12:56 +0500

    push

--- components/containers/Navbar/index.jsx
+++ components/containers/Navbar/index.jsx
@@ -227,29 +227,6 @@ export default function Navbar({ logo, categories, imagePath, blog_list }) {
                           </div>
                         </Link>
                       ))}
-
-                      <div className="col-span-2 mt-2 pt-2 border-t border-gray-100 text-center">
-                        <Link
-                          href="/categories"
-                          className="inline-flex items-center gap-1 text-primary hover:text-primary/80 font-medium"
-                        >
-                          View all categories
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="16"
-                            height="16"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            stroke="currentColor"
-                            strokeWidth="2"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                          >
-                            <line x1="5" y1="12" x2="19" y2="12"></line>
-                            <polyline points="12 5 19 12 12 19"></polyline>
-                          </svg>
-                        </Link>
-                      </div>
                     </div>
                   )}
                 </div>
